191d0a4e28a8a82e75979718989dd018d766b59a
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-08 15:47:03 +0700

    Move fabric load in TableMap

--- src/components/TableLayout/view.jsx
+++ src/components/TableLayout/view.jsx
@@ -4,21 +4,12 @@ import s from "./style"
 
 import TableCate from "../TableCate/view"
 import TableMap from "../TableMap/view"
-import Script from "react-load-script"
 
 class TableLayout extends React.PureComponent {
 
-  state = {
-    fabricLoaded: false
-  }
 
-  notifyFabricLoaded = loaded => () => {
-    console.log(`Fabric loaded: ${loaded}`)
-    this.setState({fabricLoaded: loaded})
-  }
 
   render(){
-    const url = "https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.7.19/fabric.min.js"
 
     return (
       <div style={s.rootDiv}>
@@ -27,11 +18,7 @@ class TableLayout extends React.PureComponent {
           <TableCate/>
           <TableMap/>
         </div>
-        <Script
-          url={url}
-          onLoad={this.notifyFabricLoaded(true)}
-          onError={this.notifyFabricLoaded(false)}
-        />
+
       </div>
     )
   }
--- src/components/TableMap/view.jsx
+++ src/components/TableMap/view.jsx
@@ -1,11 +1,31 @@
 import React from "react"
 import s from "./style"
 
+import Script from "react-load-script"
+
+
 class TableMap extends React.PureComponent {
+  state = {
+    fabricLoaded: false
+  }
+
+  notifyFabricLoaded = loaded => () => {
+    console.log(`Fabric loaded: ${loaded}`)
+    this.setState({fabricLoaded: loaded})
+  }
+
+
   render(){
+    const url = "https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.7.19/fabric.min.js"
+
     return (
       <div style={s.rootDiv}>
         <div style={s.header}>TableMap</div>
+        <Script
+          url={url}
+          onLoad={this.notifyFabricLoaded(true)}
+          onError={this.notifyFabricLoaded(false)}
+        />
       </div>
     )
   }
